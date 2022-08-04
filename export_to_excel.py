c40559e7d4409f2e14b3d5f759a38273bf08828f
Justin Hawley <justin@orcagis.com>
2022-08-04 01:16:50 -0500

    Created export_to_excel.py file with boilerplate code. Installed openpyxl

---
+++ export_to_excel.py
@@ -0,0 +1,24 @@
+#-------------------------------------------------------------------------------
+# Name:        export_to_excel
+# Purpose:     Exports a layer or table to .XLSX format. User may select which fields to include in the export.
+#
+# Author:      Justin Hawley (justin@orcagis.com)
+#
+# Created:     08/04/2022
+#
+# Interpreter: /fastgisapi/venv/bin/python3
+#-------------------------------------------------------------------------------
+
+from openpyxl import Workbook
+from openpyxl.styles import PatternFill, Border, Side, Alignment, Protection, Font
+from openpyxl.utils import get_column_letter
+
+
+
+def main():
+    print('Hello Bash!')
+
+
+if __name__ == '__main__':
+    main()
+
