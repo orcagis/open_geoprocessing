fcdf90658b3f9064c7b64aee701d2624bc4de696
Justin Hawley <justin@orcagis.com>
2022-08-06 12:26:18 -0500

    added file upload to specified folder on server

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 
 app = FastAPI()
 
+@app.get('/')
+async def root():
+    return{'message': 'Hello Bash!'}
+
+# shapefiles and geodatabase feature classes require zip files
+@app.post("/upload-file/")
+async def create_upload_file(uploaded_file: UploadFile = File(...)):
+    file_location = f"input/{uploaded_file.filename}"
+    with open(file_location, "wb+") as file_object:
+        file_object.write(uploaded_file.file.read())
+    return {"info": f"file '{uploaded_file.filename}' saved at '{file_location}'"}
