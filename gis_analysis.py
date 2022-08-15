9c24f385e074adcdbc5e712bc47a6cd931fdc9b2
Justin Hawley <justin@orcagis.com>
2022-08-14 20:33:35 -0500

    added export to excel

--- gis_analysis.py
+++ gis_analysis.py
@@ -22,6 +22,23 @@ def list_fields(layer, print_to_screen = False):
             print(fdefn.name)
     return (schema)
 
+def get_lyr_attributes(layer, fields, print_to_screen = False):
+    id = 0
+    output_data = []
+    for feat in layer:
+        record = {}
+        record['id'] = id
+        for field in fields:
+            val = feat.GetField(field)
+            record[field] = val
+            if print_to_screen:
+                print (val, end=', ')
+        output_data.append(record)
+        id += 1
+        if print_to_screen:
+            print()
+    return output_data
+
 # returns coordinates as a json response and optionally prints to screen
 def get_pt_coords(pt_lyr, print_to_screen = False):
     lyr_def = pt_lyr.GetLayerDefn()
@@ -44,9 +61,7 @@ def get_pt_coords(pt_lyr, print_to_screen = False):
                 "y": y
             }
             output_coords.append(record)
-
             id += 1
-
     return output_coords
 
 def main():
--- gis_tools.py
+++ gis_tools.py
@@ -8,6 +8,7 @@
 #
 # Interpreter: /fastgisapi/venv/bin/python3
 #-------------------------------------------------------------------------------
+from dataclasses import field
 import gis_input
 import gis_analysis
 import gis_output
@@ -22,10 +23,34 @@ def get_lyr_coords(path, layer_name = False, out_name = None, out_path = None, o
           gis_output.export_xlsx(lyr_coords, out_name, out_path, headers = True)
      return lyr_coords
 
-def main():
+def list_lyr_fields(path, layer_name = False, print_to_screen = False):
+     lyr = gis_input.get_layer(path,layer_name)
+     lyr_fields = gis_analysis.list_fields(lyr[0], print_to_screen)
+     return lyr_fields
 
-     my_coords = get_lyr_coords(r'/Users/justinhawley/Desktop/my_gdb.gdb','centroids',out_name = 'coords_export_gdb', out_path = '/Users/justinhawley/repos/fastgisapi/output', out_format = 'xlsx', print_to_screen = True)
-     my_coords = get_lyr_coords(r'/Users/justinhawley/Desktop/counties/centroids.shp', out_name = 'coords_export', out_path = '/Users/justinhawley/repos/fastgisapi/output', out_format = 'xlsx', print_to_screen = True)
+def export_lyr_attributes(path, layer_name = False, fields = [], out_name = None, out_path = None, out_format = 'json', print_to_screen = False):
+     lyr = gis_input.get_layer(path,layer_name)
+     if not fields:
+          fields = gis_analysis.list_fields(lyr[0])
+     lyr_attributes = gis_analysis.get_lyr_attributes(lyr[0],fields, print_to_screen)
+
+     if out_format == 'csv':
+          gis_output.export_csv(lyr_attributes, out_name, out_path, headers = True)
+     elif out_format == 'xlsx':
+          gis_output.export_xlsx(lyr_attributes, out_name, out_path, headers = True)
+     return lyr_attributes
+
+def main():
+     #my_fields = list_lyr_fields(r'/Users/justinhawley/Desktop/counties/centroids.shp', print_to_screen = True)
+
+     my_attributes = export_lyr_attributes(r'/Users/justinhawley/Desktop/counties/centroids.shp', fields = None, out_name = 'centroids_data', out_path = '/Users/justinhawley/repos/fastgisapi/output', out_format = 'csv', print_to_screen = False)
+     print(my_attributes)
+     
+     #export_lyr_attributes(r'/Users/justinhawley/Desktop/counties/centroids.shp', fields = None, out_name = 'centroids_data', out_path = '/Users/justinhawley/repos/fastgisapi/output', out_format = 'json', print_to_screen = True)
+
+
+     #my_coords = get_lyr_coords(r'/Users/justinhawley/Desktop/my_gdb.gdb','centroids',out_name = 'coords_export_gdb', out_path = '/Users/justinhawley/repos/fastgisapi/output', out_format = 'xlsx', print_to_screen = True)
+     #my_coords = get_lyr_coords(r'/Users/justinhawley/Desktop/counties/centroids.shp', out_name = 'coords_export', out_path = '/Users/justinhawley/repos/fastgisapi/output', out_format = 'csv', print_to_screen = True)
 
 if __name__ == '__main__':
     main()
